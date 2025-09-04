6e9f2120f922220b7ded52753079dab83c90f014
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2025-09-04 14:08:50 +0630

    styling

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -21,7 +21,7 @@ export default function Playlist({ playlist, currentIndex, onSelectTrack }: Play
   return (
     <div className="bg-gray-800 p-6 rounded-lg h-full flex flex-col">
       <h3 className="font-bold text-xl mb-4">Your Playlist:</h3>
-      <div className="overflow-y flex-1 pr-1 -mr-1">
+      <div className="overflow-y-auto flex-1 pr-1 -mr-2 custom-scrollbar">
         <table className="w-full">
           <tbody>
             {playlist.map((track, i) => (
@@ -34,7 +34,7 @@ export default function Playlist({ playlist, currentIndex, onSelectTrack }: Play
                 }`}
                 onClick={() => onSelectTrack(i)}
               >
-                <td className="py-3 pr-2">
+                <td className="py-3 px-3 pr-2">
                   <div className="flex items-center">
                     {i === currentIndex && (
                       <div className="mr-2 text-blue-400">
